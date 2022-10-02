ea1c42547fd336901007284ae7e0a1fcc4947c5a
Matheus Graciki <85004422+MatheusGraciki@users.noreply.github.com>
2022-10-02 12:08:06 -0300

    Update Code.C

--- Code.C
+++ Code.C
@@ -2,20 +2,25 @@
 DHTesp dhtSensor;
 #include "WiFi.h"
 #include <HTTPClient.h>
+// setting the Wifi
 const char* ssid = "Wokwi-GUEST";
-const char* password = ""; 
+const char* password = "";
+
 const String url = "https://api.thingspeak.com/update?api_key=0OOTEQH9IUW733XE&";
 HTTPClient http;
 void setup() {
   Serial.begin(115200);
   Serial.println("starting setup");
-  // leds will be start turned off
+  // Leds will be start turned off
   digitalWrite(23, LOW);
   digitalWrite(22, LOW);
 
+  // Outputs pins, the energy will get out of this pins
   dhtSensor.setup(13, DHTesp::DHT22);
   pinMode(22, OUTPUT);
   pinMode(23, OUTPUT);
+
+  // Connecting to Wifi
   WiFi.begin(ssid, password);
  
 
@@ -33,7 +38,7 @@ void setup() {
 void loop() {
  
   delay(1000);
-  // get temperature and humidity
+  // Get temperature and humidity
   float temperature = dhtSensor.getTemperature();
   float humidity = dhtSensor.getHumidity();
   Serial.println("Temperature:" + String(temperature) + "°" + " Humidity:" + String(humidity)+"%");
@@ -44,18 +49,21 @@ void loop() {
   int httpCode = http.GET();
   String payload = http.getString();
 
-  Serial.println("HttpCode:" + String(httpCode));
-  Serial.println("Payload:" + payload);
+  // Status of request send to API
+    // Serial.println("HttpCode:" + String(httpCode));
+    
+  // the object recived of the API
+    // Serial.println("Payload:" + payload);
 
 
  
-//  if  the request return a status 404 or other error, then the red led will turn on
+//  If  the request return a status 404 or other error, then the red led will turn on
   if(httpCode != 200 ){
     digitalWrite(23, LOW);
     digitalWrite(22, HIGH);
   }
 
-  // if  the request return a  status 200, then the green led will turn on
+  // If  the request return a  status 200, then the green led will turn on
   else{
     digitalWrite(23, HIGH);
     digitalWrite(22, LOW);
